6bdc2218ee0dc6885b769c614c94708381f5a18f
AndreIFCE <71884720+AndreIFCE@users.noreply.github.com>
2020-10-01 11:25:44 -0300

    Update 1087.c

--- 1087.c
+++ 1087.c
@@ -2,10 +2,10 @@
 
 int main(){
     
-	// primeira posicao
+	// Primeira posicao
 	int x1, y1;
 
-	// segunda posicao
+	// Segunda posicao
 	int x2, y2;
 
 	while ((scanf("%d %d %d %d", &x1, &y1, &x2, &y2)) && (x1 !=0 || y1 != 0 || x2 != 0 || y2 != 0)) 
